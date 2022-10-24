4e06d797d66ef1351c86c2188d819c834e3a4c06
Tudor Petrescu <if20376@bristol.ac.uk>
2022-10-24 19:48:24 +0100

    WORKBOOK 4 DONE FFS IT TOOK AGES

--- CompGraphCW/src/RedNoise.cpp
+++ CompGraphCW/src/RedNoise.cpp
@@ -23,6 +23,7 @@
 #define HEIGHT 240
 
 
+std::vector<double> zBuffer;
 
 std::unordered_map<std::string,Colour> readColours(const std::string &filename){
 
@@ -44,31 +45,42 @@ std::unordered_map<std::string,Colour> readColours(const std::string &filename){
 
 }
 
+ std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues){
+
+	std::vector<glm::vec3> result;
+	glm::vec3 numVal = glm::vec3(numberOfValues-1,numberOfValues-1,numberOfValues-1);
+	glm::vec3 stepValues =  (to-from)/numVal;
+	result.push_back(from);
+	for(int i = 1; i< numberOfValues; i++){
+
+			from = from + stepValues;
+			result.push_back(from);
+			
+		}
+
+		return result;
+	
+}
+
 glm::vec2 getCanvasIntersectionPoint(glm::vec3 cameraPos, float focalLength, glm::vec3 vertex){
 
 	vertex = vertex - cameraPos;
-	glm::vec3 scaling = glm::vec3(170,170,1);
-	vertex = vertex * scaling;
-
-	return glm::vec2((focalLength *  -vertex.x/vertex.z + WIDTH/2),focalLength * vertex.y/vertex.z+ HEIGHT/2);
+	float u = focalLength *  vertex.x/vertex.z;
+	float v = focalLength * vertex.y/vertex.z;
+	
+	u = -u *180.0 + WIDTH/2.0;
+	v = v *180.0 + HEIGHT/2.0;
+	return glm::vec2(u,v);
+	
 	
 }
-glm::vec2 pointCloudRenderer(glm::vec3 coordinates, DrawingWindow &window,float focalLength, glm::vec3 cameraPos){
-
-	
-		
-		glm::vec2 p1 = getCanvasIntersectionPoint(cameraPos,focalLength,coordinates);
-		 return p1;
-
-	}
-
-
 
 std::vector<ModelTriangle> readOBJFile(const std::string &filename,float scalingFactor,std::unordered_map<std::string,Colour> colourMap,float focalLength, glm::vec3 cameraPos, DrawingWindow &window ){
 
 
 	std::ifstream inputStream(filename);
 	std::string line;
+
 	std::vector<ModelTriangle> objTriangles;
 	std::vector<glm::vec3> coordinates;
 	Colour currentColour;
@@ -82,14 +94,12 @@ std::vector<ModelTriangle> readOBJFile(const std::string &filename,float scaling
 			currentColour = colourMap[colourName];
 		}
 		if(splitLine[0] == "v"){
-
 			float x = std::stof(splitLine[1]) * scalingFactor;
 			float y = std::stof(splitLine[2]) * scalingFactor;
 			float z = std::stof(splitLine[3]) * scalingFactor;
 			coordinates.push_back(glm::vec3(x,y,z));
-			//pointCloudRenderer(glm::vec3(x,y,z),window,focalLength,cameraPos);
-		}
-
+		} 
+		
 		else if(splitLine[0] == "f"){
 			int point1 =std::stoi(splitLine[1])-1;
 			int point2 =std::stoi(splitLine[2])-1;
@@ -122,13 +132,17 @@ std::vector<float> interpolateSingleFloats(float from, float to, int numberOfVal
 	return result;
 
 }
- void drawLine(CanvasPoint from, CanvasPoint to, DrawingWindow &window, Colour colour){
-
+#include <stdio.h>
+ void drawLine(CanvasPoint from, CanvasPoint to, DrawingWindow &window, Colour colour){		
 		float xDiff = to.x - from.x;
 		float yDiff = to.y - from.y;
+		float zDiff = to.depth - from.depth;
 		float noOfSteps = std::fmax(abs(xDiff), abs(yDiff));
 		float xStepSize = xDiff/noOfSteps;
 		float yStepSize = yDiff/noOfSteps;
+		float zStepSize = zDiff/noOfSteps;
+		
+
 
 		float red = colour.red;
 		float green = colour.green;
@@ -139,7 +153,21 @@ std::vector<float> interpolateSingleFloats(float from, float to, int numberOfVal
 
 			float x = from.x + (xStepSize * i);
 			float y = from.y + (yStepSize * i);
-			window.setPixelColour(round(x),round(y),pixelColour);
+			float z = from.depth + (zStepSize* i);
+
+			int ix = int(x);
+			int iy = int(y);
+
+			double prev_depth = zBuffer[ix + iy * WIDTH];
+
+			if ((1.0 / z) >= prev_depth) {
+				window.setPixelColour(ix, iy, pixelColour);
+				zBuffer[ix + iy * WIDTH] = (1.0 / z);
+			}
+
+
+				
+			
 		}
 
  }
@@ -151,6 +179,56 @@ void drawTriangle(CanvasTriangle tr, DrawingWindow &window, Colour colour){
 
 }
 
+void drawDepthFilledTriangle(CanvasTriangle tr, DrawingWindow &window, Colour colour){
+
+	CanvasPoint top = tr.v0();
+	CanvasPoint bottom = tr.v1();
+	CanvasPoint right = tr.v2();
+	
+	if(top.y > bottom.y){
+
+		std::swap(top, bottom);
+
+	}
+	if(top.y > right.y){
+		std::swap(top, right);
+	}
+
+	if(bottom.y < right.y){
+		std::swap(bottom,right);
+	}
+
+	float red = colour.red;
+	float green = colour.green;
+	float blue = colour.blue;
+	uint32_t pixelColour = (255 << 24) + (int(red)<<16) + (int(green)<<8 ) + (int(blue));
+
+	float x = top.x - (top.x-bottom.x)*(right.y-top.y)/(bottom.y-top.y);
+	std::vector<glm::vec3> topRight = interpolateThreeElementValues(glm::vec3(top.x,top.y,top.depth),glm::vec3(right.x,right.y,right.depth),abs(right.y-top.y+1));
+	std::vector<glm::vec3> topToNew = interpolateThreeElementValues(glm::vec3(top.x,top.y,top.depth),glm::vec3(x,right.y,bottom.depth),abs(right.y-top.y+1));
+
+	for(int i = 0; i < topRight.size(); i++){
+
+		glm::vec3 left = topToNew.at(i);
+		glm::vec3 right = topRight.at(i);
+		
+		drawLine(CanvasPoint(left.x,left.y),CanvasPoint(right.x,right.y),window,colour);
+	}
+
+
+	std::vector<glm::vec3> NewtoBottom = interpolateThreeElementValues(glm::vec3(x,right.y,top.depth),glm::vec3(bottom.x,bottom.y,bottom.depth),abs(right.y-bottom.y+1));
+	std::vector<glm::vec3> RightToBottom = interpolateThreeElementValues(glm::vec3(right.x,right.y,top.depth),glm::vec3(bottom.x,bottom.y,bottom.depth),abs(right.y-bottom.y+1));
+
+	for(int i = 0; i < NewtoBottom.size(); i++){
+
+		glm::vec3 left = NewtoBottom.at(i);
+		glm::vec3 right = RightToBottom.at(i);
+	
+
+		drawLine(CanvasPoint(left.x,left.y),CanvasPoint(right.x,right.y),window,colour);
+	}
+
+}
 void drawFilledTriangle(CanvasTriangle tr, DrawingWindow &window, Colour colour){
 
 	CanvasPoint top = tr.v0();
@@ -177,51 +255,89 @@ void drawFilledTriangle(CanvasTriangle tr, DrawingWindow &window, Colour colour)
 	CanvasPoint newPoint = CanvasPoint(x,y);
 
 	drawTriangle(tr,window,colour);
-	drawLine(right,newPoint,window,colour);
+	float  z = top.depth - (top.depth - bottom.depth) * (y-top.y)/(bottom.y-top.y);
 	
+
 	for(float Y = top.y; Y < y; Y++){
+
 
 		float leftX = top.x - (top.x - x)*(Y-top.y)/(y- top.y);
 		float rightX = top.x - (top.x - right.x)*(Y - top.y)/(right.y - top.y);
 
-		drawLine(CanvasPoint(leftX,Y),CanvasPoint(rightX,Y),window,colour);
+		float leftZ = top.depth - (top.depth - z) * (Y-top.y)/(y- top.y);
+		float rightZ = top.depth- (top.depth-right.depth) *(Y - top.y)/(right.y - top.y);
+
+		drawLine(CanvasPoint(leftX,Y,leftZ),CanvasPoint(rightX,Y,rightZ),window,colour);
 	}
 
 	for(float Y = newPoint.y; Y < bottom.y; Y++){
 
 		float leftX = newPoint.x - (newPoint.x - bottom.x)*(Y-newPoint.y)/(bottom.y-newPoint.y);
 		float rightX = right.x - (right.x - bottom.x)*(Y - right.y)/(bottom.y - right.y);
-		drawLine(CanvasPoint(leftX,Y),CanvasPoint(rightX,Y),window,colour);
+
+		float leftZ = z - ( z - bottom.depth ) * (Y-newPoint.y)/(bottom.y-newPoint.y);
+		float rightZ = right.depth - (right.depth - bottom.depth) *(Y - right.y)/(bottom.y - right.y);
+
+		drawLine(CanvasPoint(leftX,Y, leftZ),CanvasPoint(rightX,Y, rightZ),window,colour);
 	}
 
 
 }
 
-void wireFrameRender(std::vector<ModelTriangle> vertices, DrawingWindow &window, float focalLength, glm::vec3 cameraPos,bool colour){
+std::vector<CanvasTriangle> convertModelToCanvas(std::vector<ModelTriangle> vertices, DrawingWindow &window, float focalLength, glm::vec3 cameraPos){
+
+	std::vector<CanvasTriangle> canvasTriangles;
 
 	for(ModelTriangle tr : vertices){
 
-		glm::vec2 p1 = pointCloudRenderer(tr.vertices[0],window,focalLength,cameraPos);
-		glm::vec2 p2 = pointCloudRenderer(tr.vertices[1],window,focalLength,cameraPos);
-		glm::vec2 p3 = pointCloudRenderer(tr.vertices[2],window,focalLength,cameraPos);
-		
 
-		if(colour==false){
+		float z1 = tr.vertices[0].z - cameraPos.z;
+		float z2 = tr.vertices[1].z - cameraPos.z;
+		float z3 = tr.vertices[2].z - cameraPos.z;
 
-		drawTriangle(CanvasTriangle(CanvasPoint(p1.x,p1.y),CanvasPoint(p2.x,p2.y),CanvasPoint(p3.x,p3.y)),window,Colour(255,255,255));
-
-		}
+		glm::vec2 p1 = getCanvasIntersectionPoint(cameraPos,focalLength,tr.vertices[0]);
+		glm::vec2 p2 = getCanvasIntersectionPoint(cameraPos,focalLength,tr.vertices[1]);
+		glm::vec2 p3 = getCanvasIntersectionPoint(cameraPos,focalLength,tr.vertices[2]);		
 	
-		else{
 
-		drawFilledTriangle(CanvasTriangle(CanvasPoint(p1.x,p1.y),CanvasPoint(p2.x,p2.y),CanvasPoint(p3.x,p3.y)),window,tr.colour);
+		// float z1 = (tr.vertices[0].z - cameraPos.z) * (tr.vertices[0].z - cameraPos.z);
+		// float z2 = (tr.vertices[1].z - cameraPos.z) * (tr.vertices[1].z - cameraPos.z);
+		// float z3 = (tr.vertices[2].z - cameraPos.z) * (tr.vertices[2].z - cameraPos.z);
 
-		}
-	
+		canvasTriangles.push_back(CanvasTriangle(CanvasPoint(p1.x,p1.y,abs(z1)),CanvasPoint(p2.x,p2.y,abs(z2)),CanvasPoint(p3.x,p3.y,abs(z3))));
 	}
+
+
+	return canvasTriangles;
+
 }
 
 
+void initialiseDepthBuffer(float **zBuffer, int width, int height){
+
+	for(int i = 0; i< WIDTH;i++){
+
+		for(int j = 0; j< HEIGHT;j++){
+
+			zBuffer[i][j] = 0;
+
+		}
+
+	}
+
+}
+
+void wireFrameRender(std::vector<ModelTriangle> vertices, DrawingWindow &window, float focalLength, glm::vec3 cameraPos,bool colour) {	
+	std::vector<CanvasTriangle> canvasTriangles = convertModelToCanvas(vertices,window,focalLength,cameraPos);
+
+	for(int i = 0; i< canvasTriangles.size();i++){
+		if(colour==false){
+			drawTriangle(canvasTriangles[i],window,Colour(255,255,255));
+		} else{
+			drawFilledTriangle(canvasTriangles[i],window,vertices[i].colour);
+		}		
+	}
+}
 
 void drawTexturedTriangle(CanvasTriangle canvasTriangle, CanvasTriangle textureTriangle,DrawingWindow &window, TextureMap textureMap){
 
@@ -243,7 +359,6 @@ void drawTexturedTriangle(CanvasTriangle canvasTriangle, CanvasTriangle textureT
 	CanvasPoint right = canvasTriangle.v2();
 	
 	if(top.y > bottom.y){
-
 		std::swap(top, bottom);
 
 	}
@@ -300,22 +415,6 @@ void drawTexturedTriangle(CanvasTriangle canvasTriangle, CanvasTriangle textureT
 
 
 }
- std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues){
-
-	std::vector<glm::vec3> result;
-	glm::vec3 numVal = glm::vec3(numberOfValues-1,numberOfValues-1,numberOfValues-1);
-	glm::vec3 stepValues =  (to-from)/numVal;
-	result.push_back(from);
-	for(int i = 1; i< numberOfValues; i++){
-
-			from = from + stepValues;
-			result.push_back(from);
-			
-		}
-
-		return result;
-	
-}
 
 void draw(DrawingWindow &window) {
 	window.clearPixels();
@@ -332,10 +431,9 @@ void draw(DrawingWindow &window) {
 
 		std::vector<glm::vec3> rowInterpol = interpolateThreeElementValues(left.at(y),right.at(y),WIDTH);
 		for (size_t x = 0; x < window.width; x++) {
-			// if(i == WIDTH)
-			// i = 0;
+			
 			 uint32_t colour = (255 << 24) + (int(rowInterpol.at(x).x) << 16) + ((int(rowInterpol.at(x).y)) << 8) + (int(rowInterpol.at(x).z));
-			// i++;
+			
 
 			
 			 window.setPixelColour(x, y, colour);
@@ -346,7 +444,7 @@ void draw(DrawingWindow &window) {
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
-	if (event.type == SDL_KEYDOWN) {
+	if (event.type == SDL_KEYDOWN){
 		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
@@ -380,31 +478,26 @@ int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
 	srand(time(NULL));
+	
 
-
+	zBuffer = std::vector<double>(WIDTH * HEIGHT, 0.0);
+	
 	glm::vec3 cameraPos = glm::vec3(0.0,0.0,4.0);
 	TextureMap map = TextureMap("src/texture.ppm");
 	std::unordered_map<std::string,Colour> colourMap = readColours("src/cornell-box.mtl");
-	std::vector<ModelTriangle> test = readOBJFile("src/cornell-box.obj",0.35,colourMap,2,cameraPos,window);
+	std::vector<ModelTriangle> modelTriangles = readOBJFile("src/cornell-box.obj",0.35,colourMap,2,cameraPos,window);
 
+	bool colour = true;
 
-	// for(ModelTriangle mtr: test){
-	// 	std::cout<<mtr<<" "<<mtr.colour<<" "<<std::endl;
-	// }
-	//  for (auto x : colourMap){
-
-    // std::cout << x.first << " " <<
-    //         x.second << std::endl;
-	// 		}	
+	
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
 		//draw(window);
-		//pointCloudRenderer(test,window,2,cameraPos);
-		wireFrameRender(test,window,2,cameraPos,true);
-		// drawTexturedTriangle(CanvasTriangle(CanvasPoint(160, 10),CanvasPoint(300, 230),CanvasPoint(10, 150)),
-		// CanvasTriangle(CanvasPoint(195, 5),CanvasPoint(395, 380),CanvasPoint(65, 330)),window,map);
+		wireFrameRender(modelTriangles,window,2,cameraPos,colour);
 		
+
+		zBuffer = std::vector<double>(WIDTH * HEIGHT, 0.0);		
 
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
